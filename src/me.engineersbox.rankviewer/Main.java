fc78e5cc2f43d951ec5c750de0ab922f8956b1e1
Jack Kilrain <toastcraft.info@gmail.com>
2018-06-17 08:20:13 +0200

    resolved message error for default users

--- src/me.engineersbox.rankviewer/Main.java
+++ src/me.engineersbox.rankviewer/Main.java
@@ -64,46 +64,90 @@ public class Main extends JavaPlugin implements Listener {
 		
 		PermissionUser user2 = PermissionsEx.getUser(p);
 		
-		List<String> groups = new ArrayList<>();
-		List<String> g2 = user2.getOwnParentIdentifiers();
-		String prefix = format(user2.getPrefix());
 		String username = user2.getName();
-		for (int i = 0; i < g2.size(); i++) {
-			groups.add(g2.get(i).toString());
-		}
 		
 		String ChatMessage = e.getMessage();
 		e.setCancelled(true);
 		
-		TextComponent comp = new TextComponent(ChatColor.AQUA + "[" + ChatColor.RED + "R" + ChatColor.AQUA + "] " + ChatColor.WHITE);
-		
-		ArrayList<Object> components = new ArrayList<>();
-		TextComponent hoverMessage = new TextComponent(new ComponentBuilder(groups.get(0)).create());
-		TextComponent newLine = new TextComponent(ComponentSerializer.parse("{text: \"\n\"}"));
-		
-		for (int i = 1; i < groups.size(); i++) {
+		if ((user2.inGroup("default")) && (user2.getOwnParentIdentifiers().size() < 1)) {
 			
-			hoverMessage.addExtra(newLine);
-			hoverMessage.addExtra(new TextComponent(new ComponentBuilder(groups.get(i)).create()));
+			/*p.sendMessage("no groups");
+			List<String> g1 = user2.getOwnParentIdentifiers();
+			for (int i = 0; i < g1.size(); i++) {
+				p.sendMessage(g1.get(i).toString());
+			}
+			p.sendMessage(g1.toString());
+			*/ //debug
 			
-		}
-		
-		components.add(hoverMessage);
-		BaseComponent[] hoverToSend = (BaseComponent[])components.toArray(new BaseComponent[components.size()]);
-		
-		comp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverToSend));
-		
-		TextComponent comp2 = new TextComponent(prefix + username + ChatColor.WHITE +": ");
-		comp2.addExtra(ChatMessage);
-
-		for (Player player : Bukkit.getOnlinePlayers()) {
+			String prefixDefault = format(user2.getPrefix());
 			
-			player.spigot().sendMessage(comp, comp2);
-			e.setMessage(ChatMessage);
-
-		}
+			ArrayList<Object> components2 = new ArrayList<>();
+			
+			TextComponent rTab = new TextComponent(ChatColor.AQUA + "[" + ChatColor.RED + "R" + ChatColor.AQUA + "] " + ChatColor.WHITE);
+			TextComponent hoverMessage2 = new TextComponent(new ComponentBuilder(prefixDefault + "No Rank").create());
+			
+			components2.add(hoverMessage2);
+			BaseComponent[] hoverToSend2 = (BaseComponent[])components2.toArray(new BaseComponent[components2.size()]);
+			
+			rTab.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverToSend2));
+			
+			TextComponent comp2 = new TextComponent(prefixDefault + username + ChatColor.WHITE +": ");
+			comp2.addExtra(ChatMessage);
+			
+			for (Player player : Bukkit.getOnlinePlayers()) {
+				
+				player.spigot().sendMessage(rTab, comp2);
+				e.setMessage(ChatMessage);
+	
+			}
+			
+			DiscordUtil.sendMessage(DiscordSRV.getPlugin().getMainTextChannel(), username + " » " + ChatMessage);
+			
+		} else {
+			
+			//p.sendMessage("Has groups");	//debug
 		
-		DiscordUtil.sendMessage(DiscordSRV.getPlugin().getMainTextChannel(), "**" + groups.get(0).toString() + "** " + username + " » " + ChatMessage);
+			List<String> groups = new ArrayList<>();
+			List<String> g2 = user2.getOwnParentIdentifiers();
+			List<String> groupPrefix = new ArrayList<>();
+			String prefix = format(user2.getPrefix());
+			
+			for (int i = 0; i < g2.size(); i++) {
+				groups.add(g2.get(i).toString());
+				groupPrefix.add(format(user2.getParents().get(i).getPrefix()));
+			}
+			
+			ArrayList<Object> components = new ArrayList<>();
+			
+			TextComponent comp = new TextComponent(ChatColor.AQUA + "[" + ChatColor.RED + "R" + ChatColor.AQUA + "] " + ChatColor.WHITE);
+			TextComponent hoverMessage = new TextComponent(new ComponentBuilder(groupPrefix.get(0) +groups.get(0)).create());
+			TextComponent newLine = new TextComponent(ComponentSerializer.parse("{text: \"\n\"}"));
+			
+			for (int i = 1; i < groups.size(); i++) {
+				
+				hoverMessage.addExtra(newLine);
+				hoverMessage.addExtra(new TextComponent(new ComponentBuilder(groupPrefix.get(i) + groups.get(i)).create()));
+				
+			}
+			
+			components.add(hoverMessage);
+			BaseComponent[] hoverToSend = (BaseComponent[])components.toArray(new BaseComponent[components.size()]);
+			
+			comp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverToSend));
+			
+			TextComponent comp2 = new TextComponent(prefix + username + ChatColor.WHITE +": ");
+			comp2.addExtra(ChatMessage);
+	
+			for (Player player : Bukkit.getOnlinePlayers()) {
+				
+				player.spigot().sendMessage(comp, comp2);
+				e.setMessage(ChatMessage);
+	
+			}
+			
+			DiscordUtil.sendMessage(DiscordSRV.getPlugin().getMainTextChannel(), "**" + groups.get(0).toString() + "** " + username + " » " + ChatMessage);
+		
+    	}
 		
 	}
     
